911e52b0d997a88c4b6b2d567a9cb7960d4f0421
Tom-ash04 <tomy04@vp.pl>
2017-04-19 17:03:58 +0200

    adding triangle calculation

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,10 @@
 // plik scripts.js
-
-var name = prompt('Wpisz swoje imię');
+var a, h, triangleArea;
+var a = prompt("Wpisz wymiar podstawy trójkąta");
+var h = prompt("Wpisz wymiar wysokości trójkąta");
+triangleArea = a*h/2;
 //alert('Witaj, ' + name);
-console.log('Witaj, ' + name);
+console.log("Pole trójkąta o podstawie a: " + a + " i wysokości h: " + h + " wynosi: " + triangleArea);
 
 //linear comment
 
